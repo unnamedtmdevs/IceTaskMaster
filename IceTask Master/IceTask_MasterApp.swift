7d76cf257fa3dd3e2a0695fd0f065b2ec9995050
ludik <bakhanetssimon@gmail.com>
2025-12-03 18:42:23 +0300

    Initial Commit

---
+++ IceTask Master/IceTask_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceTask_MasterApp.swift
+//  IceTask Master
+//
+//  Created by Simon Bakhanets on 03.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct IceTask_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IceTask MasterTests/IceTask_MasterTests.swift
@@ -0,0 +1,16 @@
+//
+//  IceTask_MasterTests.swift
+//  IceTask MasterTests
+//
+//  Created by Simon Bakhanets on 03.12.2025.
+//
+
+import Testing
+
+struct IceTask_MasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
